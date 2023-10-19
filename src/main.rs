4f3f8a7bb4ab3361773469a579878bd82a80514e
kn1dar1an <arthur.maya21@gmail.com>
2023-10-19 20:24:45 +0300

    Add async runtime base

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-mod ui;
+use std::{thread, time::Duration};
 
-use ui::Ui;
+use async_runtime::AsyncRuntime;
+
+use crate::async_runtime::async_task::AsyncTaskResult;
+
+pub mod ui;
+mod async_runtime;
 
 fn main() {
-    let window: Ui = ui::create_ui();
-    ui::destroy_ui();
+    let runtime = AsyncRuntime::new();
+
+    match runtime {
+        Ok(mut rt) => {
+            rt.start().unwrap();
+            let spawner = rt.get_spawner().unwrap();
+            let mut count = 0;
+            loop {
+                if count == 5 { break; }
+                let result = spawner.spawn_non_blocking( async {
+                    println!("Hello!");
+                    AsyncTaskResult::Ok
+                });
+                match result {
+                    Err(err) => {println!("{}", err)}
+                    _ => {}
+                }
+                count += 1;
+                thread::sleep(Duration::new(2, 0));
+            }
+
+            rt.stop();
+        }
+        Err(_msg) => { panic!("{_msg}"); }
+    }
+
 }
